f34caad2f388e867a6ca25af325b0cb6ca10240e
ALiao1432 <apple36578@gmail.com>
2018-06-15 17:33:27 +0800

    set style to NoActionBar

--- app/src/main/java/com/example/ian/mycompass/MainActivity.java
+++ app/src/main/java/com/example/ian/mycompass/MainActivity.java
@@ -33,6 +33,7 @@ import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
